fdc674c601149551a221ac65c6dfd41dbdc24a6a
QuillanW <56843492+QuillanW@users.noreply.github.com>
2023-04-18 14:40:53 +0200

    Update

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -9,18 +9,32 @@ public class CameraControl : MonoBehaviour
     public bool inControl = false;
     public bool animate = true;
 
+    Vector3 wantedRotation;
+    float scrollPosition = -80;
+
     public float cameraSpeed = 1;
+
+    private void Start()
+    {
+        wantedRotation = transform.rotation.eulerAngles;
+    }
 
     private void Update()
     {
-        Vector3 newRotation = transform.rotation.eulerAngles;
+        if (inControl && !animate)
+        {
+            //Camera zoom
+            scrollPosition += Input.GetAxis("Scroll") * Time.deltaTime * 2500 * cameraSpeed;
+            scrollPosition = Mathf.Clamp(scrollPosition,-80,-10);
+            playerCamera.transform.localPosition = new Vector3(0, 0, scrollPosition);
+        }
 
         if (Input.GetMouseButton(1) && inControl && !animate)
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
 
-            newRotation += new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"),0);
+            wantedRotation += new Vector3(-Input.GetAxis("Mouse Y") * 500 * Time.deltaTime * cameraSpeed, Input.GetAxis("Mouse X") * 500 * Time.deltaTime * cameraSpeed, 0);
         } 
         else
         {
@@ -30,12 +44,15 @@ public class CameraControl : MonoBehaviour
 
         if (animate && !inControl)
         {
-            newRotation += new Vector3(0,Time.deltaTime * 10,0);
+            wantedRotation += new Vector3(0, 10 * Time.deltaTime, 0);
         }
 
         if (animate || inControl)
         {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(newRotation), cameraSpeed * Time.deltaTime);
+            //Limit camera rotation
+            wantedRotation = new Vector3(Mathf.Clamp(wantedRotation.x,10,90),wantedRotation.y,0);
+            //Set the rotation
+            transform.rotation = Quaternion.Euler(wantedRotation);
         }
     }
 }
