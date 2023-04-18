7a084ca5dc2c4b69f699bc007de92aa2006729ab
kris195 <77070538+kris195@users.noreply.github.com>
2023-04-18 16:12:38 +0200

    Continue

--- Assets/Scripts/pointer.cs
+++ Assets/Scripts/pointer.cs
@@ -18,7 +18,7 @@ public class pointer : MonoBehaviour
         
     }
     public IEnumerator run() {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         Destroy(gameObject);
 
     }
